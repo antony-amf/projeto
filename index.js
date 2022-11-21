51b0988c7bbb9a6ee297c02cc393841cf44a330d
Antony Melo <ameloferreira.amf@gmail.com>
2022-11-21 17:08:31 -0300

    Arquivo index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Importação do express
+const express = require('express')
+const app = express()
+
+
+app.get('/',function(req, res){
+   res.send("Sistema Ativo")
+})
+
+app.listen(3000, function(){ 
+    console.log("Inicialização na Porta 3000")
+})
